858be8646c44f69064c03a11679d8b02d2e3d2a8
VegetaPrince8 <moryo.t719@gmail.com>
2023-03-02 21:35:11 +0900

    favicon commit

--- resources/views/layouts/map-parent.blade.php
+++ resources/views/layouts/map-parent.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('img/main-logo_favicon.ico') }}">
+    <link rel="apple-touch-icon" href="{{ asset('img/main-logo_favicon.ico') }}">
     <title>@yield("title")</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/destyle.css@3.0.2/destyle.css">
--- resources/views/layouts/parent.blade.php
+++ resources/views/layouts/parent.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield("title")</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" href="{{ asset('img/main-logo_favicon.ico') }}">
     <link rel="apple-touch-icon" href="{{ asset('img/main-logo_favicon.ico') }}">
+    <title>@yield("title")</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/destyle.css@3.0.2/destyle.css">
     <link href="{{ asset('css/header.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/front.css') }}" rel="stylesheet">
